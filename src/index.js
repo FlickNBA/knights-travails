a041f089576405f8a6f15768bfeca96db6cd42d1
FlickNBA <flicknba@icloud.com>
2023-07-03 21:52:13 +0200

    Board + CSS ready :)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./reset.css";
 import "./index.css";
+
+import { initBoard } from "./initBoard";
+
+initBoard();
---
+++ src/initBoard.js
@@ -0,0 +1,32 @@
+export const initBoard = () => {
+  let helperLeft = document.querySelector("#helper-left");
+  let helperBottom = document.querySelector("#helper-bottom");
+  let board = document.querySelector("#board");
+
+  board.style.width = `${Math.min(document.body.clientWidth, document.body.clientHeight) * 0.65}px`;
+  board.style.height = `${Math.min(document.body.clientWidth, document.body.clientHeight) * 0.65}px`;
+  helperLeft.style.height = `${Math.min(document.body.clientWidth, document.body.clientHeight) * 0.65}px`;
+  helperBottom.style.width = `${Math.min(document.body.clientWidth, document.body.clientHeight) * 0.65}px`;
+
+  [..."87654321"].forEach((row, rowIndex) => {
+    let helper = document.createElement("div");
+    helper.classList = `left-${row}`;
+    helper.innerHTML = `${row}`;
+    helperLeft.append(helper);
+
+    [..."ABCDEFGH"].forEach((column, columnIndex) => {
+      let tile = document.createElement("div");
+      tile.id = `${column}-${row}`;
+      tile.classList = `tile-${row}`;
+      //tile.innerHTML = `${column}${row}`;
+      board.appendChild(tile);
+    });
+  });
+
+  [..."ABCDEFGH"].forEach((column) => {
+    let helper = document.createElement("div");
+    helper.classList = `bottom-${column}`;
+    helper.innerHTML = `${column}`;
+    helperBottom.append(helper);
+  });
+};
